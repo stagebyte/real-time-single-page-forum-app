a210460162e9b765131a3de9130d5c1a324f7835
Stanley <stagebyte45@gmail.com>
2021-09-18 16:20:17 +0100

    Factory and Seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Database\Seeder;
 
+use App\User;
+use App\Model\Category;
+use App\Model\Question;
+use App\Model\Reply;
+use App\Model\Like;
+
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -12,5 +19,16 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        //create 10 users
+        factory(User::class, 10)->create();
+        //create 5 categories
+        factory(Category::class, 5)->create();
+        //create 10 questions
+        factory(Question::class, 10)->create();
+        //create 50 replies
+        factory(Reply::class, 50)->create()->each(function ($reply) {
+            // make each user like a reply.
+            return $reply->like()->save(factory(Like::class)->make());
+        });
     }
 }
